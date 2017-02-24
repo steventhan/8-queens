b5c74352d938b591019cce5c1d98975b1c2643ab
Steven Than <steventhan11@gmail.com>
2017-02-24 18:35:14 -0500

    done board class, start solving 8-queens problem

---
+++ python/8-queen.py
@@ -0,0 +1,14 @@
+from board import Chessboard
+
+
+def main():
+    """main function"""
+    pass
+
+
+def find_solutions():
+
+
+
+if __name__ == '__main__':
+    main()
--- python/board.py
+++ python/board.py
@@ -5,30 +5,53 @@ Solving the 8 queens problem in python
 class UnsafeException(Exception):
     pass
 
+
+class QueenNotFoundException(Exception):
+    pass
+
+
 class Chessboard(object):
     """simulates a Chessboard."""
     def __init__(self, n=8):
         self.board = [[0] * n for _ in range(n)]
-
-    def total_solutions(self):
-        pass
+        self.total_solutions = 0
 
     def is_safe(self, row, col):
         """checks if location is safe"""
         return not self.board[row][col]
 
-    def _change_row(self, row, option='unsafe', idx=0):
-        if idx == len(self.board[row]) - 1:
-            self.board[row][idx] += 1 if option == 'unsafe' else -1
+    def _change_row(self, row, unsafe=True, idx=0):
+        if idx == len(self.board):
             return
-        self.board[row][idx] += 1 if option == 'unsafe' else -1
-        return self._change_row(row, option, idx=(idx+1))
+        self.board[row][idx] += 1 if unsafe else -1
+        return self._change_row(row, unsafe, idx=(idx+1))
 
-    def _change_col(self, col, option="unsafe"):
-        pass
+    def _change_col(self, col, unsafe=True, idx=0):
+        if idx == len(self.board):
+            return
+        self.board[idx][col] += 1 if unsafe else - 1
+        return self._change_col(col, unsafe, idx=(idx+1))
 
-    def _change_diag(self, row, col, option="unsafe"):
-        pass
+    def _change_diag(self, row, col, unsafe=True):
+        last_idx = len(self.board) - 1
+        posn_left = (0, (col-row)) if row < col else ((row-col), 0)
+        posn_right = (0, col + (row - 0)) if (row - 0) < (last_idx - col) else (row - (last_idx - col), last_idx)
+
+        def _change_diag_left(self, row, col, unsafe):
+            if any(val == len(self.board) for val in (row, col)):
+                return
+            self.board[row][col] += 1 if unsafe else -1
+            return _change_diag_left(self, (row+1), (col+1), unsafe)
+
+        def _change_diag_right(self, row, col, unsafe):
+            if row == len(self.board) or col < 0:
+                return
+            self.board[row][col] += 1 if unsafe else -1
+            # import pdb; pdb.set_trace()
+            return _change_diag_right(self, (row+1), (col-1), unsafe)
+
+        _change_diag_left(self, posn_left[0], posn_left[1], unsafe)
+        _change_diag_right(self, posn_right[0], posn_right[1], unsafe)
 
     def place(self, row, col):
         """places a queen at the specified location"""
@@ -42,7 +65,13 @@ class Chessboard(object):
 
     def remove(self, row, col):
         """removes the queen at the location"""
-        pass
+        if isinstance(self.board[row][col], Queen):
+            self._change_row(row, unsafe=False)
+            self._change_col(col, unsafe=False)
+            self._change_diag(row, col, unsafe=False)
+            self.board[row][col] = 0
+            return
+        raise QueenNotFoundException('Queen not found')
 
 
 class Queen(object):
@@ -50,11 +79,11 @@ class Queen(object):
     def __init__(self):
         pass
 
+    def __add__(self, n):
+        return self
+
     def __str__(self):
         return "Q"
-
-    def __add__(self, other):
-        return self
 
     def __repr__(self):
         return self.__str__()

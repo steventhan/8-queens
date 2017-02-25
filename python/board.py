856d17707a20745cfcf521f0c8d73dfe78376ca7
Steven Than <steventhan11@gmail.com>
2017-02-24 19:10:02 -0500

    add __len__ to board class

--- python/board.py
+++ python/board.py
@@ -14,37 +14,41 @@ class Chessboard(object):
     """simulates a Chessboard."""
     def __init__(self, n=8):
         self.board = [[0] * n for _ in range(n)]
-        self.total_solutions = 0
+        self.num_solutions = 0
+        self.num_queens = 0
+
+    def __len__(self):
+        return len(self.board)
 
     def is_safe(self, row, col):
         """checks if location is safe"""
         return not self.board[row][col]
 
     def _change_row(self, row, unsafe=True, idx=0):
-        if idx == len(self.board):
+        if idx == len(self):
             return
         self.board[row][idx] += 1 if unsafe else -1
         return self._change_row(row, unsafe, idx=(idx+1))
 
     def _change_col(self, col, unsafe=True, idx=0):
-        if idx == len(self.board):
+        if idx == len(self):
             return
         self.board[idx][col] += 1 if unsafe else - 1
         return self._change_col(col, unsafe, idx=(idx+1))
 
     def _change_diag(self, row, col, unsafe=True):
-        last_idx = len(self.board) - 1
+        last_idx = len(self) - 1
         posn_left = (0, (col-row)) if row < col else ((row-col), 0)
         posn_right = (0, col + (row - 0)) if (row - 0) < (last_idx - col) else (row - (last_idx - col), last_idx)
 
         def _change_diag_left(self, row, col, unsafe):
-            if any(val == len(self.board) for val in (row, col)):
+            if any(val == len(self) for val in (row, col)):
                 return
             self.board[row][col] += 1 if unsafe else -1
             return _change_diag_left(self, (row+1), (col+1), unsafe)
 
         def _change_diag_right(self, row, col, unsafe):
-            if row == len(self.board) or col < 0:
+            if row == len(self) or col < 0:
                 return
             self.board[row][col] += 1 if unsafe else -1
             # import pdb; pdb.set_trace()
@@ -60,6 +64,7 @@ class Chessboard(object):
             self._change_row(row)
             self._change_col(col)
             self._change_diag(row, col)
+            self.num_queens += 1
             return
         raise UnsafeException('Unsafe cell')
 
